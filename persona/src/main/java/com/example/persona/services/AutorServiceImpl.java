e87b702544d7cf5a382fbd3396ba3b26deb0ab46
Flores_Abel <calamardoaf@hotmail.com>
2022-09-24 15:29:21 -0300

    apiRest JAVA

--- persona/src/main/java/com/example/persona/services/AutorServiceImpl.java
+++ persona/src/main/java/com/example/persona/services/AutorServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.persona.services;
 import com.example.persona.entities.Autor;
 import com.example.persona.entities.Base;
 import com.example.persona.repositoires.BaseRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
@@ -11,4 +12,6 @@ public class AutorServiceImpl extends BaseServiceImpl<Autor,Long> implements Aut
     public AutorServiceImpl(BaseRepository<Autor, Long> baseRepository) {
         super(baseRepository);
     }
+
+
 }
--- persona/src/main/java/com/example/persona/services/BaseServiceImpl.java
+++ persona/src/main/java/com/example/persona/services/BaseServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.persona.services;
 
 import com.example.persona.entities.Base;
 import com.example.persona.repositoires.BaseRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
@@ -15,6 +17,7 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     public BaseServiceImpl(BaseRepository<E, ID> baseRepository) {
         this.baseRepository = baseRepository;
     }
+
     @Override
     @Transactional // PARA PODER COMUNICARME CON LA BASE DE DATOS
     public List<E> findAll() throws Exception {
@@ -24,6 +27,18 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
         }catch (Exception e){
             throw new Exception(e.getMessage());
         }
+    }
+
+    @Override
+    @Transactional
+    public Page<E> findAll(Pageable pageable) throws Exception{
+        try{
+            Page<E>entities= baseRepository.findAll(pageable);
+            return entities;
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+
     }
 
     @Override
